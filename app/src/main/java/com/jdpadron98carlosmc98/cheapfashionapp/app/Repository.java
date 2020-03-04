a8505a153080e227cbd22d00d2e8d34c9229d77e
Jose Daniel <jdpadron98@gmail.com>
2020-03-04 19:33:47 +0000

    T1. Diseñar Login
    Creado layout para la orientacion landscape.

--- app/src/main/java/com/jdpadron98carlosmc98/cheapfashionapp/app/Repository.java
+++ app/src/main/java/com/jdpadron98carlosmc98/cheapfashionapp/app/Repository.java
@@ -2,17 +2,10 @@ package com.jdpadron98carlosmc98.cheapfashionapp.app;
 
 import android.content.Context;
 
-import java.util.ArrayList;
-
 public class Repository implements Contract {
 
     private static Repository INSTANCE;
     private Context context;
-
-    private ArrayList<String> gi50List = new ArrayList<>();
-    private ArrayList<String> cell_lineList = new ArrayList<>();
-    private ArrayList<String> id_expList = new ArrayList<>();
-    private ArrayList<String> restList = new ArrayList<>();
 
 
     public static Contract getInstance(Context context) {
