ef508f1db75a3f9891ed1ac9664fceee69c1f451
Carlos <carlosmesac98@gmail.com>
2020-05-01 12:25:45 +0100

    Crear JSON:
    Modificaciones para coger el JSON de interntet de nuestra base de datos.

--- app/src/main/java/com/jdpadron98carlosmc98/cheapfashionapp/app/Repository.java
+++ app/src/main/java/com/jdpadron98carlosmc98/cheapfashionapp/app/Repository.java
@@ -1,19 +1,27 @@
 package com.jdpadron98carlosmc98.cheapfashionapp.app;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.util.Log;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.jdpadron98carlosmc98.cheapfashionapp.Home.HomeActivity;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -24,7 +32,7 @@ public class Repository implements RepositoryContract {
     private Context context;
     public static String TAG = Repository.class.getSimpleName();
 
-    public static final String JSON_FILE = "defaultCatalog.json";
+    public static final String JSON_FILE = "https://cheap-fashion-app.firebaseio.com/.json";
     public static final String JSON_ROOT = "defaultProducts";
 
 
@@ -69,7 +77,7 @@ public class Repository implements RepositoryContract {
         productItemList.add(productItem4);
         productItemList.add(productItem5);
         productItemList.add(productItem6);*/
-        loadCatalogFromJSON(loadJSONFromAsset());
+        new JsonTask().execute(JSON_FILE);
 
     }
 
@@ -115,5 +123,65 @@ public class Repository implements RepositoryContract {
         } catch (JSONException error) {
         }
         return false;
+    }
+
+
+
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        protected String doInBackground(String... params) {
+            HttpURLConnection connection = null;
+            BufferedReader reader = null;
+
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+
+                InputStream stream = connection.getInputStream();
+
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuffer buffer = new StringBuffer();
+                String line = "";
+
+                while ((line = reader.readLine()) != null) {
+                    buffer.append(line+"\n");
+                    Log.d("Response: ", "> " + line);   //here u ll get whole response...... :-)
+
+                }
+
+                return buffer.toString();
+
+
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+            loadCatalogFromJSON(result);
+        }
     }
 }
